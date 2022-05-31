bb2de39703cad6faa5cc99c3452af11fa1f57466
Hays4Days <gerushays@gmail.com>
2022-05-30 19:08:23 -0600

    figure out write file

--- src/page-template.js
+++ src/page-template.js
@@ -48,7 +48,7 @@ const generateTeam = team => {
                  <ul class="list-group">
                     <li class="list-group-item">ID: ${intern.getId()}</li>
                      <li class="list-group-item">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
-                    <li class="list-group-item">Office Number: ${intern.getOfficeNumber()}</li>
+                    <li class="list-group-item">School: ${intern.getSchool()}</li>
                 </ul>
              </div>
         </div>
